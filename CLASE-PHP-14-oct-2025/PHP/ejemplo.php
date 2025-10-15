86f4f5cabef331277f06d0998030cc2b71c50e36
Salas de Sistemas ETITC <salasistemas@etitc.local>
2025-10-14 19:47:53 -0500

    Nuevo tema: PHP

---
+++ CLASE-PHP-14-oct-2025/PHP/ejemplo.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $nombre = "Diego";
+        $edad = 23;
+        $direccion = null;
+
+        // OPERADORES
+        $a = 10;
+        $b = 4;
+        $suma = $a + $b;
+        $multiplicacion = $a * $b;
+
+        // MOSTRAR RESULTADOS
+        echo "<p> Hola, $nombre, Tienes $edad años </p>"
+        echo "El precio del producto es: $ $precio </p>"
+        echo "La suma de $a y $b es: $suma </p>"
+
+    ?>
+</body>
+</html> 
